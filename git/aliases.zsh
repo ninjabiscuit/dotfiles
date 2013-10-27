1a38b9653309abeb2237662cc00659f0cbbeb82c
ninjabiscuit <andy@madebymany.co.uk>
2013-10-27 10:12:39 +0000

    some git alises and removed unnecessary files

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,6 +8,8 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
+alias ga='git add'
+alias gall='git-all'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
